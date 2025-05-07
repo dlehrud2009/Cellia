44372d7f36425bb33d5900f1da09922a506a5aef
Cellia <ffffff.gogle@gmail.com>
2025-05-07 18:05:03 +0900

    main

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 col1, col2 = st.columns([2,3])
 
-with col1 :
+with col1:
     st.title('here is column1')
-with col2 :
-    st.title('here is column2')
-    st.checkbox('this is checkbox2 in col2')
+    st.subheader('i am column1 subheader')
 
-    col1.subheader(' i am column1 subheader')
-    col2.checkbox('this is checkbox2 in col2')  
+with col2:
+    st.title('here is column2')
+    st.checkbox('this is checkbox2 in col2', key='checkbox2_1')
+    st.checkbox('this is checkbox2 in col2', key='checkbox2_2')
